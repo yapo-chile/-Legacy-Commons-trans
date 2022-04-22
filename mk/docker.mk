b349c9b985d2e463eaaaa09846a5e4a4db9a82fc
Dany Jaque <djaque@gmail.com>
2022-04-22 14:24:29 -0400

    ref/gitlab - refator make targets

--- mk/docker.mk
+++ mk/docker.mk
@@ -1,11 +1,3 @@
-## Create docker image based on docker/dockerfile
-docker-build:
-	@scripts/commands/docker-build.sh
-
-## Push docker image to containers.mpi-internal.com
-docker-publish:
-	@scripts/commands/docker-publish.sh
-
 ## Attach to this service's currently running docker container output stream
 docker-attach:
 	@scripts/commands/docker-attach.sh
--- mk/vars.mk
+++ mk/vars.mk
@@ -4,14 +4,11 @@ export GO_FILES = $(shell find . -iname '*.go' -type f | grep -v vendor | grep -
 GENPORTOFF?=0
 genport = $(shell expr ${GENPORTOFF} + \( $(shell id -u) - \( $(shell id -u) / 100 \) \* 100 \) \* 200 + 30100 + $(1))
 
-# BRANCH info from travis
-export BUILD_BRANCH=$(shell if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then echo "${TRAVIS_BRANCH}"; else echo "${TRAVIS_PULL_REQUEST_BRANCH}"; fi)
-
 # GIT variables
 export BRANCH=$(shell git branch | sed -n 's/^\* //p')
-export GIT_BRANCH=$(shell if [ -n "${BUILD_BRANCH}" ]; then echo "${BUILD_BRANCH}"; else echo "${BRANCH}"; fi;)
 export GIT_COMMIT=$(shell git rev-parse HEAD)
 export GIT_COMMIT_DATE=$(shell TZ="America/Santiago" git show --quiet --date='format-local:%d-%m-%Y_%H:%M:%S' --format="%cd")
+export COMMIT_DATE_UTC ?= $(shell TZ=UTC git show --quiet --date='format-local:%Y%m%d_%H%M%S' --format="%cd")
 export BUILD_CREATOR=$(shell git log --format=format:%ae | head -n 1)
 
 # REPORT_ARTIFACTS should be in sync with `RegexpFilePathMatcher` in
@@ -22,9 +19,10 @@ export REPORT_ARTIFACTS=reports
 # This variables are for the use of your microservice. This variables must be updated each time you are creating a new microservice
 export APPNAME=trans
 export YO=`whoami`
-export SERVICE_HOST=:$(call genport,2)
+export SERVICE_HOST=localhost
+export SERVICE_PORT=8086
 export SERVER_ROOT=${PWD}
-export BASE_URL="http://${SERVICE_HOST}"
+export BASE_URL="http://${SERVICE_HOST}:${SERVICE_PORT}"
 export MAIN_FILE=cmd/${APPNAME}/main.go
 export LOGGER_SYSLOG_ENABLED=false
 export LOGGER_SYSLOG_IDENTITY=trans
@@ -37,15 +35,13 @@ export LOGGER_LOG_LEVEL=0
 # TRANS_HOST=jenna.schibsted.cl
 # TRANS_PORT=27205
 
-# Pact test variables
-export PACT_MAIN_FILE=cmd/${APPNAME}-pact/main.go
-export PACT_BINARY=${APPNAME}-pact
-
 # DOCKER variables
 export DOCKER_REGISTRY=gitlab.com/yapo_team/legacy/commons/${APPNAME}
-export DOCKER_TAG=$(echo ${GIT_BRANCH} | tr '[:upper:]' '[:lower:]' | sed 's,/,_,g')
+
+export DOCKER_TAG=$(shell echo ${BRANCH} | tr '[:upper:]' '[:lower:]' | sed 's,/,_,g')
 export DOCKER_IMAGE=${DOCKER_REGISTRY}
 export DOCKER_PORT=$(call genport,1)
+export DOCKER ?= docker
 
 # Documentation variables
 export DOCS_DIR=docs
